024300da3176ece9e0e269ff3c7cb6f835b107b8
tuguldur <aa402115@gmail.com>
2020-01-31 02:12:30 +0800

    Updates

---
+++ precache-manifest.2c60340e1c4cbd2cf1d01f0817d299e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573357f8130c98934591abc888c9dba6",
+    "url": "/fume-survey/index.html"
+  },
+  {
+    "revision": "27fe5c9d714971e4efa0",
+    "url": "/fume-survey/static/css/2.b2bd93e9.chunk.css"
+  },
+  {
+    "revision": "5a7223944ca382290ffa",
+    "url": "/fume-survey/static/css/main.c5e468c4.chunk.css"
+  },
+  {
+    "revision": "27fe5c9d714971e4efa0",
+    "url": "/fume-survey/static/js/2.41df4386.chunk.js"
+  },
+  {
+    "revision": "88d7d5fae2230bcd4212cbaffe0fbb14",
+    "url": "/fume-survey/static/js/2.41df4386.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a7223944ca382290ffa",
+    "url": "/fume-survey/static/js/main.e58cae66.chunk.js"
+  },
+  {
+    "revision": "8b87e0a96780c24a0b6a",
+    "url": "/fume-survey/static/js/runtime-main.75b94461.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/fume-survey/precache-manifest.2c60340e1c4cbd2cf1d01f0817d299e4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fume-survey/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
